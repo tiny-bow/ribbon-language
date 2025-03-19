1b08c8319259999264f92b2807380013b4a8dd6f
noxabellus <noxabellus@gmail.com>
2025-03-19 00:17:01 -0500

    simplified watcher script

--- src/bin/tools/watcher.sh
+++ src/bin/tools/watcher.sh
@@ -2,21 +2,4 @@
 
 clear
 
-echo "Rebuilding..."
-
-output=$(zig build run -freference-trace --prominent-compile-errors 2>&1)
-
-if [ $? -eq 0 ]; then
-    clear
-    echo "Rebuild succeeded"
-else
-    clear
-    echo "$output" \
-        | grep -i "error" -m 1 -A 8 \
-        | awk -v total_lines=$(echo "$output" | wc -l) '
-            {print}
-            END {
-                if (NR < total_lines) print "... (use single build for more info)"
-            }'
-    exit 1
-fi
+zig build run -freference-trace --prominent-compile-errors
