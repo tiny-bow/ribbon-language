f3a6f620a2508c6057b0d388f8e20c0243f6facd
nox <noxabellus@gmail.com>
2025-05-13 18:42:47 -0500

    implemented ir2bc table mirrors

--- src/mod/ir2bc.zig
+++ src/mod/ir2bc.zig
@@ -12,7 +12,6 @@ test {
     std.testing.refAllDeclsRecursive(@This());
 }
 
-
 /// Compilation state for the whole program, kept alive between `Jit` compilation jobs.
 pub const Compiler = struct {
     /// the allocator to use for compiler intermediate data
@@ -22,19 +21,10 @@ pub const Compiler = struct {
     /// can be appended to, but not (yet, anyway) mutated;
     /// not all IR is necessarily compiled at any given time.
     ir: *const ir.Builder,
-    // TODO the following types from core contain static buffers, but we will need extensible buffers.
-    // theres a bunch of ways we could handle this such as:
-    // * grab a larger buffer from `allocator`; expand as necessary, like an ArrayList but manual
-    // * create a mirror type using actual arraylists
-    // * use a fixed size buffer and just assume we won't need more than that
-    // * use virtual memory and commit pages as needed
-    // ways we shouldnt handle it include:
-    // * rewrite existing type using actual arraylists
-    //   > core is meant to be the static binary representation of the language
     /// binds fully qualified names to bytecode ids, for the whole program compiled so far
-    symbol_table: core.SymbolTable,
+    symbol_table: SymbolTable,
     /// binds bytecode ids to addresses, for the whole program compiled so far
-    address_table: core.AddressTable,
+    address_table: AddressTable,
 };
 
 /// Data local to the compilation of a single function. Created by `Compiler`, reusable.
@@ -43,9 +33,58 @@ pub const Jit = struct {
     root: *const Compiler,
     /// the ir for the function being compiled
     ir: *const ir.Function,
-    // TODO same as above but different considerations: we probably just use a fixed size buffer here; its temp data.
     /// binds fully qualified names to bytecode ids for the function being compiled
-    symbol_table: core.SymbolTable,
+    symbol_table: SymbolTable,
     /// binds bytecode ids to addresses for the function being compiled
-    address_table: core.AddressTable,
+    address_table: AddressTable,
+};
+
+
+/// A mirror of `core.SymbolTable` that is extensible.
+pub const SymbolTable = pl.StringMap(core.SymbolTable.Value);
+
+/// A mirror of `core.AddressTable` that is extensible.
+pub const AddressTable = struct {
+    /// Constant value bindings section.
+    constants: pl.ArrayList(*const core.Constant) = .empty,
+    /// Global value bindings section.
+    globals: pl.ArrayList(*const core.Global) = .empty,
+    /// Function value bindings section.
+    functions: pl.ArrayList(*const core.Function) = .empty,
+    /// Builtin function value bindings section.
+    builtin_addresses: pl.ArrayList(*const core.BuiltinAddress) = .empty,
+    /// C ABI value bindings section.
+    foreign_addresses: pl.ArrayList(*const core.ForeignAddress) = .empty,
+    /// Effect handler set bindings section.
+    handler_sets: pl.ArrayList(*const core.HandlerSet) = .empty,
+    /// Effect identity bindings section.
+    effects: pl.ArrayList(*const core.Effect) = .empty,
+
+    // validate mirror
+    comptime {
+        outer: for (std.meta.fieldNames(core.AddressTable)) |core_field| {
+            for (std.meta.fieldNames(AddressTable)) |comp_field| {
+                if (std.mem.eql(u8, core_field, comp_field)) continue :outer;
+            } else {
+                @compileError("missing field " ++ core_field ++ " in ir2bc.AddressTable");
+            }
+        }
+
+        outer: for (std.meta.fieldNames(AddressTable)) |comp_field| {
+            for (std.meta.fieldNames(core.AddressTable)) |core_field| {
+                if (std.mem.eql(u8, core_field, comp_field)) continue :outer;
+            } else {
+                @compileError("extra field " ++ comp_field ++ " in ir2bc.AddressTable");
+            }
+        }
+
+        for (std.meta.fieldNames(AddressTable)) |field| {
+            const our_type = @typeInfo(@FieldType(@FieldType(AddressTable, field), "items")).pointer.child;
+            const core_type = @FieldType(core.AddressTable, field).ValueType;
+
+            if (our_type != core_type) {
+                @compileError("type mismatch for field " ++ field ++ ": expected " ++ @typeName(core_type) ++ ", got " ++ @typeName(our_type));
+            }
+        }
+    }
 };
